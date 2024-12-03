74c98d047426eff1444d0ccd3e75e61a5d19c851
lennartjakob <lennart@lennartjakob.com>
2024-12-03 10:22:55 +0100

    restarted 2

--- 2/parttwo.js
+++ 2/parttwo.js
@@ -10,33 +10,18 @@ var safe = 0;
 /* Problematic Case:
 2
 77 80 81 84 85 86 86 90 is unsafe
+
+wenn 2. unsicher, wird direction falsch erkannt
 */ 
+
+var reports = [];
 
 input.split("\n").forEach(e => {
   const report = e.split(" ");
-
-  var unsafeOperations = 0;
-  const directionsCount = [0,0]
-
-  var last;
-  var lastDir;
-  const entryCount = report.length;
-
-  for (let i = 0; i < entryCount; i++) {
-    const entry = report[i];
-    if (i != 0) {
-      if (error) {
-        i = 0;
-      } else {
-      }
-    }
-    last = entry
-  }
-
-
-  if (unsafeOperations <= 1) {
-    safe++;
-  }
+  reports.push(report);
 });
+
+
+
 
 console.log(safe);
